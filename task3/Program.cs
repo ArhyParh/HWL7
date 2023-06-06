21396658fab4d5cc01011b4f2b9b48dc1649d374
ArhyParh <zescoarty@gmail.com>
2023-06-06 21:35:47 +0300

    Решил 3тью и 4ю задачу

--- task3/Program.cs
+++ task3/Program.cs
@@ -5,3 +5,75 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+Console.Clear();
+Random rnd = new Random();
+Main();
+void Main()
+{
+    Console.WriteLine("Введите количество строк и столбцов 1 массива, через пробел..");
+    int [] first = Array.ConvertAll(Console.ReadLine()!.Split()!, int.Parse);
+    Console.WriteLine("Введите количество строк и столбцов 2 массива, через пробел..");
+    int [] second = Array.ConvertAll(Console.ReadLine()!.Split()!, int.Parse);
+    int [,] res1 = GetArray(first[0],first[1],1,10);
+    int [,] res2 = GetArray(second[0],second[1],1,10);
+    Console.WriteLine();
+    PrintArray(res1);
+    Console.WriteLine();
+    PrintArray(res2);
+    Console.WriteLine();
+    if(CanWeMulti(res1,res2)==true){PrintArray(MultiplicationArray(res1,res2));}
+    else{Console.Write("Данные матрицы не подлежат перемножению.");}
+
+
+}
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] back = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            back[i, j] = rnd.Next(minValue, maxValue + 1);
+
+        }
+    }
+    return back;
+
+}
+void PrintArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} |");
+        }
+        Console.WriteLine();
+    }
+}
+Boolean CanWeMulti(int[,] first, int [,] second)
+{
+    if(first.GetLength(1)==second.GetLength(0)) {return true;}
+    else return false;
+}
+int[,] MultiplicationArray(int[,] first, int[,] second)
+{
+    int[,] back =new int [first.GetLength(0),second.GetLength(1)];
+    {
+        for (int i = 0; i < back.GetLength(0); i++)
+    {
+        for (int j = 0; j < back.GetLength(1); j++)
+        {
+            for (int n = 0; n < first.GetLength(1); n++)
+            {
+                back[i,j] += first[i,n]*second[n,j];
+            }
+            
+        }
+    }
+
+    }
+    return back;
+
+}
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.Clear();
+Random rnd = new Random();
+Main();
+void Main()
+{
+    Console.WriteLine("Введите x,y,z массива через пробел...");
+    int [] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+    int [] temp = FillFullArr();
+    if(arr[0]*arr[1]*arr[2]<=90) 
+    {int [,,] res = GetArray(arr[0],arr[1],arr[2],temp);
+    PrintArray(res);}
+    else Console.WriteLine("Для данной задачи размерность слишком велика");
+    
+}
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i,j,k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+int [,,] GetArray(int x, int y, int z, int [] one)
+{
+    int [,,] back = new int[x,y,z];
+    for (int i = 0; i < back.GetLength(0); i++)
+    {
+        for (int j = 0; j < back.GetLength(1); j++)
+        {
+            for (int k = 0; k < back.GetLength(2); k++)
+            {
+                back[i,j,k] = ChangePos(one);
+            }
+        }
+    }
+    return back;
+}
+Boolean CanWeDoThis(int[] arr, int num)
+{
+    if(arr[num]!=0){return true;}
+    else return false;
+}
+int [] FillFullArr()
+{
+    int [] one = new int[90];
+    for (int i = 0; i <90; i++)
+    {
+        one[i] =10+i;
+    }
+    
+    return one;
+}
+int ChangePos(int [] arr)
+{
+    int p = rnd.Next(0,90);
+
+ if(CanWeDoThis(arr,p)==true)
+ {
+   arr[p]=0;
+   return p; 
+ }
+ else return ChangePos(arr);
+ 
+}
